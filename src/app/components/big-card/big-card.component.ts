d32f9d3c9408ce35e089973b2392751502f2f506
italofvm <italofernandesvm@gmail.com>
2023-08-28 14:06:21 -0300

    Adicionando Responsividade

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css', './big-card.responsive.component.css'],
+})
+export class BigCardComponent {}
---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: [
+    './small-card.component.css',
+    'small-card.responsive.component.css',
+  ],
+})
+export class SmallCardComponent {}
