1b487a4c229a9e55ecf847ac689189acf342c013
italofvm <italofernandesvm@gmail.com>
2023-08-28 15:14:26 -0300

    components dinamycs propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
     'small-card.responsive.component.css',
   ],
 })
-export class SmallCardComponent {}
+export class SmallCardComponent {
+  @Input()
+  photoCover: string = '';
+
+  @Input()
+  CardTitle: string = '';
+}
